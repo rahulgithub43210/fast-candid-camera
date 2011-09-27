d0d6bde156efd4fc01323b11c0f0e3405572a419
evenpm@gmail.com <evenpm@gmail.com@f4e7b24b-5de9-e236-0442-7bd63a562279>
2011-09-27 06:01:53 +0000

    去掉context 全局变量，无使用情况

--- trunk/fast-candid-camera/FastCandidCamera/src/com/wucl/candid_camera/Preview.java
+++ trunk/fast-candid-camera/FastCandidCamera/src/com/wucl/candid_camera/Preview.java
@@ -21,17 +21,19 @@ import android.widget.ImageView.ScaleType;
  * @author wuchenliang
  * 
  */
-class Preview extends SurfaceView implements SurfaceHolder.Callback {
+class Preview extends SurfaceView implements SurfaceHolder.Callback
+{
 	private SurfaceHolder holder;
 	private Camera camera;
 	private ImageView ivFocus;
-	private Context context;
-	CameraPreview cameraPreview;
+	private CameraPreview cameraPreview;
 	// 创建一个PictureCallback对象，并实现其中的onPictureTaken方法
-	private PictureCallback pictureCallback = new PictureCallback() {
+	private PictureCallback pictureCallback = new PictureCallback()
+	{
 		// 该方法用于处理拍摄后的照片数据
 		@Override
-		public void onPictureTaken(byte[] data, Camera camera) {
+		public void onPictureTaken(byte[] data, Camera camera)
+		{
 
 			// data参数值就是照片数据，将这些数据以key-value形式保存，以便其他调用该Activity的程序可
 			// 以获得照片数据
@@ -45,20 +47,19 @@ class Preview extends SurfaceView implements SurfaceHolder.Callback {
 		}
 	};
 
-	// Preview类的构造方法
-	public Preview(Context context) {
-		super(context);
-		// 获得SurfaceHolder对象
-		holder = getHolder();
-		// 指定用于捕捉拍照事件的SurfaceHolder.Callback对象
-		holder.addCallback(this);
-		// 设置SurfaceHolder对象的类型
-		holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+	// // Preview类的构造方法
+	// public Preview(Context context) {
+	// super(context);
+	// // 获得SurfaceHolder对象
+	// holder = getHolder();
+	// // 指定用于捕捉拍照事件的SurfaceHolder.Callback对象
+	// holder.addCallback(this);
+	// // 设置SurfaceHolder对象的类型
+	// holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+	// }
 
-	}
-
-	public Preview(Context context, CameraPreview cameraPreview,
-			ImageView ivFocus) {
+	public Preview(Context context, CameraPreview cameraPreview, ImageView ivFocus)
+	{
 		super(context);
 		// 获得SurfaceHolder对象
 		holder = getHolder();
@@ -67,20 +68,23 @@ class Preview extends SurfaceView implements SurfaceHolder.Callback {
 		// 设置SurfaceHolder对象的类型
 		holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 		this.cameraPreview = cameraPreview;
-		this.context = context;
 		this.ivFocus = ivFocus;
 
 	}
 
 	// 开始拍照时调用该方法
-	public void surfaceCreated(SurfaceHolder holder) {
+	public void surfaceCreated(SurfaceHolder holder)
+	{
 		// 获得Camera对象
 		camera = Camera.open();
-		try {
+		try
+		{
 
 			// 设置用于显示拍照摄像的SurfaceHolder对象
 			camera.setPreviewDisplay(holder);
-		} catch (IOException exception) {
+		}
+		catch (IOException exception)
+		{
 			// 释放手机摄像头
 			camera.release();
 			camera = null;
@@ -88,21 +92,23 @@ class Preview extends SurfaceView implements SurfaceHolder.Callback {
 	}
 
 	// 停止拍照时调用该方法
-	public void surfaceDestroyed(SurfaceHolder holder) {
+	public void surfaceDestroyed(SurfaceHolder holder)
+	{
 		// 释放手机摄像头
 		camera.stopPreview();
 		camera.release();
 	}
 
 	// 拍照状态变化时调用该方法
-	public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-		try {
+	public void surfaceChanged(SurfaceHolder holder, int format, int w, int h)
+	{
+		try
+		{
 			Camera.Parameters parameters = camera.getParameters();
 			// 设置照片格式
 			parameters.setPictureFormat(PixelFormat.JPEG);
 			// 根据屏幕方向设置预浏尺寸
-			if (((Activity) context).getWindowManager().getDefaultDisplay()
-					.getOrientation() == 0)
+			if (cameraPreview.getWindowManager().getDefaultDisplay().getOrientation() == 0)
 				parameters.setPreviewSize(h, w);
 			else
 				parameters.setPreviewSize(w, h);
@@ -115,31 +121,39 @@ class Preview extends SurfaceView implements SurfaceHolder.Callback {
 			camera.startPreview();
 			// 准备用于表示对焦状态的图像（类似图14.7所示的对焦符号）
 			ivFocus.setImageResource(R.drawable.focus1);
-			LayoutParams layoutParams = new LayoutParams(
-					LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+			LayoutParams layoutParams = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
 			ivFocus.setScaleType(ScaleType.CENTER);
-			((Activity) context).addContentView(ivFocus, layoutParams);
+			cameraPreview.addContentView(ivFocus, layoutParams);
 			ivFocus.setVisibility(VISIBLE);
 			// 自动对焦
-			camera.autoFocus(new AutoFocusCallback() {
+			camera.autoFocus(new AutoFocusCallback()
+			{
 				@Override
-				public void onAutoFocus(boolean success, Camera camera) {
-					if (success) {
+				public void onAutoFocus(boolean success, Camera camera)
+				{
+					if (success)
+					{
 						// success为true表示对焦成功，改变对焦状态图像（一个绿色的png图像）
 						ivFocus.setImageResource(R.drawable.focus2);
 					}
 				}
 			});
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			camera.release();
 		}
 	}
 
 	// 停止拍照，并将拍摄的照片传入PictureCallback接口的onPictureTaken方法
-	public void takePicture() {
-		if (camera != null) {
+	public void takePicture()
+	{
+		if (camera != null)
+		{
 			camera.takePicture(null, null, pictureCallback);
-		} else {
+		}
+		else
+		{
 			Toast.makeText(getContext(), "camera==null", Toast.LENGTH_SHORT);
 		}
 	}
