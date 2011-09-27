f8d7c7cc391482ed1ea2fd932f4ea388e3796225
wcllove <wcllove@f4e7b24b-5de9-e236-0442-7bd63a562279>
2011-09-27 09:50:35 +0000

    保存文件到文件夾

--- trunk/fast-candid-camera/FastCandidCamera/src/com/wucl/candid_camera/Test.java
+++ trunk/fast-candid-camera/FastCandidCamera/src/com/wucl/candid_camera/Test.java
@@ -1,15 +1,54 @@
 package com.wucl.candid_camera;
 
+import java.io.File;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 
 public class Test extends Activity {
 
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.test);
-        
-    }
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.test);
+
+	}
+	/*
+	//Definition
+	static Uri mTempUri = null;
+	String sTempFilename = "tempfile.jpg";
+//	File path = new File(android.os.Environment.getExternalStorageDirectory(),sTempFilename);
+	mTempUri = Uri.fromFile(path);
+
+	//startActivity with extra information
+	Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	      if(mTempUri != null) {
+	         intent.putExtra(MediaStore.EXTRA_OUTPUT,mTempUri);
+	      }
+	startActivityForResult(intent, CAMERA_WITH_DATA);
+}
+
+
+
+
+
+
+//result
+protected void onActivityResult(int requestCode, int resultCode, Intent data)
+{
+if (resultCode == RESULT_OK ) {
+  if(mTempUri != null) {
+      File file = new File(android.os.Environment.getExternalStorageDirectory(),sTempFilename);
+      Uri uri = Uri.fromFile(file);
+      // here you can decode Bitmap file using BitmapFactory.decodeFile()
+  }
+}         
+}
+
+*/
 
 }
+
+
